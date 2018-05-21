ce95cbcd1ea6f22613aafa17bcda1f7196d31ec4
yinjimmy <jimmy.yin5@gmail.com>
2018-05-21 15:14:10 +0800

    improve build script

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # ln -sv /Users/Shared/Epic\ Games/UE_4.16 /Users/Shared/UnrealEngine/4.16
 
 UE_VERSION=$1
+UE_PLUGIN_NAME="XPlugin"
 if [[ $# -eq 0 ]]; then
     UE_VERSION="4.13"
 fi
@@ -14,6 +15,6 @@ echo "Building Plugin for UnrealEngine " ${UE_VERSION}
 
 CURR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-${UE_PATH}${UAT_PATH}RunUAT.sh BuildPlugin -Plugin="${CURR_PATH}/XPlugin.uplugin" -TargetPlatforms=Mac+IOS -Package="${CURR_PATH}/output"
+${UE_PATH}${UAT_PATH}RunUAT.sh BuildPlugin -Plugin="${CURR_PATH}/Plugins/${UE_PLUGIN_NAME}.uplugin" -TargetPlatforms=Mac+IOS -Package="${CURR_PATH}/output"
 
-# cp -R "${CURR_PATH}/output/Binaries" "${CURR_PATH}/Plugins/SDKBOX/chartboost/"
+cp -R "${CURR_PATH}/output/Binaries" "${CURR_PATH}/Plugins/${UE_PLUGIN_NAME}/"
